cac028cf9b8f6813e6c884a425297c34a3d0f914
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-05-27 23:00:50 +0800

    Upload Jing Zhi's files

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Garuda Angel and Mortal Bot
 
 This bot was created/previously maintained by Chan Jing Zhi (2022 HC Progs member, 2023 CSC EVP) and Ethan Cheung (2022 Garu HH).
+
+### Change Log
+
+- Upload of csv file to register players for admin users.
+- Deployment on Heroku.
+- Support for photos, stickers, documents, audio, video, and animations.
+
+## Sample CSV File
+
+```
+Player,Partner
+telehandle1,telehandle2
+```
+
+## References
+
+The bot is built using [Python Telegram Bot](https://pypi.org/project/python-telegram-bot/) library. View the docs [here](https://docs.python-telegram-bot.org/en/v13.13/).
---
+++ src/uploader.py
@@ -0,0 +1,33 @@
+import csv
+import logging
+
+import os
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+logger = logging.getLogger(__name__)
+dir = os.path.join(PARENT_DIR, 'src/creds.json')
+cred = credentials.Certificate(dir)
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+dbName = u'anm_1'
+
+with open('./csv/anm_1.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    results = ""
+    for row in csv_reader:
+        if line_count == 0:
+            logger.info(f'Column names are {", ".join(row)}.')
+            results += f'Column names are {", ".join(row)}.\n'
+            line_count += 1
+        else:
+            playerName = {"username":row[0].strip().lower(),"chatId" :None}
+            partnerName = {"username":row[1].strip().lower(),"chatId" :None}
+            db.collection(dbName).add(playerName)
+            db.collection(dbName).add(partnerName)
+
+            

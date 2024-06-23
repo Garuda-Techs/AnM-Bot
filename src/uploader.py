5b785012c3067e6e7b29248a0e89875587fc8542
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-06-23 22:06:12 +0800

    Refactor uploader.py + allow collection resets

--- src/uploader.py
+++ src/uploader.py
@@ -2,40 +2,93 @@ import csv
 import logging
 import os
 import firebase_admin
+from dotenv import load_dotenv
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+load_dotenv()
 print('UPLOADER.PY BEGINS EXECUTION')
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+pyers = os.path.join(PARENT_DIR, os.environ['CSV_PATH'])
 logger = logging.getLogger(__name__)
 dir = os.path.join(PARENT_DIR, 'src/creds.json')
+# print(dir)
 
 # Initialise an instance of Cloud Firestore on our own server.
 # Ref: https://firebase.google.com/docs/firestore/quickstart?authuser=0&hl=en#python_1
 # Use the bot's Firebase project service account credentials
 # Ref: https://cloud.google.com/iam/docs/service-account-overview
-cred = credentials.Certificate(dir)
-if not firebase_admin._apps:
-    print("Initalising Firebase app...")
-    app = firebase_admin.initialize_app(cred)
-else:
-    print("Firebase app already initialised. Continuing execution.")
-db = firestore.client()
-dbName = 'pairings'
+def initialise_firestore():
+    """ Returns a Firestore client with the necessary credentials configured.
+    
+    (None) -> Client
+    """
+    try:
+        cred = credentials.Certificate(dir)
+        if not firebase_admin._apps:
+            logger.info("Initalising Firebase app...")
+            app = firebase_admin.initialize_app(cred)
+        else:
+            logger.info("Firebase app already initialised. Continuing execution.")
+        return firestore.client()
+    except Exception as e:
+        logger.error(f'Failed to initialise Firestore: {e}')
+        logger.info('Please undeploy the A&M Bot and debug before re-deploying it.')
+        exit()
+
+def clear_firestore_collection(collection_ref, collection):
+    """ Clears the Firestore collection to prepare it for a clean CSV upload.
+    
+    (CollectionReference) -> None
+    """
+    try:
+        docs = collection_ref.stream()
+        for doc in docs:
+            doc.reference.delete()
+        logger.info(f'Cleared collection: {collection}')
+    except Exception as e:
+        logger.error(f'Failed to clear collection: {collection}')
+        logger.info('Please undeploy the A&M Bot and debug before re-deploying it.')
+
 
 def upload_data_to_firestore():
-    with open('./csv/pairings.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        results = ""
-        for (line_count, row) in enumerate(csv_reader):
-            if line_count == 0:
-                logger.info(f'Column names are {", ".join(row)}.')
-                results += f'Column names are {", ".join(row)}.\n'
-                print(results)
-            else:
-                angel_name = {"username":row[0].strip().lower(),"chatId" :None}
-                mortal_name = {"username":row[1].strip().lower(),"chatId" :None}
-                db.collection(dbName).add(angel_name)
-                db.collection(dbName).add(mortal_name)
-                print(f'Added pair {line_count} ({angel_name["username"], mortal_name["username"]}) to the database.')
+    """ Uploads all data from the pairings CSV file to Firestore.
+    
+    (None) -> None
+    """
+    db = initialise_firestore()
+    collection = 'pairings'
+    collection_ref = db.collection(collection)
+    batch = db.batch()
+    try:
+        clear_firestore_collection(collection_ref, collection)
+        logger.info("Uploading data to Firestore...")        
+        with open(pyers) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            results = ""
+            for (line_count, row) in enumerate(csv_reader):
+                if line_count == 0:
+                    logger.info(f'Column names are {", ".join(row)}.')
+                    results += f'Column names are {", ".join(row)}.\n'
+                    # print(results)
+                    continue
+                else:
+                    angel_name = {"username":row[0].strip().lower(),"chatId" :None}
+                    mortal_name = {"username":row[1].strip().lower(),"chatId" :None}
+                    batch.set(collection_ref.document(), angel_name)
+                    batch.set(collection_ref.document(), mortal_name)
+                    print(f'Prepared pair {line_count} {angel_name["username"], mortal_name["username"]} for DB commit.')
+            logger.info('Committing batch...')
+            batch.commit()
+            logger.info('Batch commit successful. Database updated.\n')
+            logger.info('CSV uploaded!')
+                
+    except Exception as e:
+        logger.error(f'Failed to upload data to Firestore: {e}')
+        logger.info('Please undeploy the A&M Bot and debug before re-deploying it.')
+        exit()
+                
+if __name__ == "__main__":
+    upload_data_to_firestore()
+    print("Terminating uploader.py...")
